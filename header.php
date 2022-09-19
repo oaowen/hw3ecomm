643ef8434d50c8a4a957125673508d46b3d33182
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-19 14:33:13 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <title>HW2 Owen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
-    <body style="background-color:powderblue;">
+    <body style="background-image: url('https://cdn.britannica.com/85/13085-050-C2E88389/Corpus-Christi-College-University-of-Cambridge-England.jpg');">
  <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
