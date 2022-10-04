8602a97b0c19b3e18e22f47f80f0e4507fb37290
oaowen <89744805+oaowen@users.noreply.github.com>
2022-10-04 17:13:55 -0500

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         <a class="nav-link" href="courses.php">Courses</a>
-        <a class="nav-link" href="#">Subject</a>
+        <a class="nav-link" href="sections.php">Section</a>
         
       </div>
     </div>
